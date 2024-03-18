16e25235b2d365d96d16a3cd81d59ddedee4454c
damongsanga <mike0413lata@gmail.com>
2024-03-18 22:41:13 +0900

    Refactor: Controller단 비즈니스 로직 service/domain 단으로 이동

--- back/bboard/src/main/java/com/ssafypjt/bboard/controller/UserGroupController.java
+++ back/bboard/src/main/java/com/ssafypjt/bboard/controller/UserGroupController.java
@@ -10,8 +10,8 @@ import com.ssafypjt.bboard.session.SessionConst;
 import com.ssafypjt.bboard.session.SessionManager;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,6 +22,7 @@ import java.util.Map;
 @RestController
 @Slf4j
 @RequestMapping("/api/user-group")
+@RequiredArgsConstructor
 public class UserGroupController {
 
     private UserService userService;
@@ -30,48 +31,39 @@ public class UserGroupController {
     private UserAddReloadDomain userAddReloadDomain;
     private SessionManager sessionManager;
 
-    @Autowired
-    private UserGroupController(UserService userService, GroupService groupService, ObjectMapper mapper, UserAddReloadDomain userAddReloadDomain, SessionManager sessionManager) {
-        this.userService = userService;
-        this.groupService = groupService;
-        this.mapper = mapper;
-        this.userAddReloadDomain = userAddReloadDomain;
-        this.sessionManager = sessionManager;
-    }
 
     // 유저 입력 페이지
-
     // 로그인
     @GetMapping("login/{userName}")
     public ResponseEntity<?> login(@PathVariable String userName,
                                    @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Integer userId,
                                    HttpServletResponse response){
+
         response.setContentType("text/html;charset=UTF-8");
+
         User user = userService.getUserByName(userName);
         if (user == null) return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+
         sessionManager.createSession(user.getUserId(), response);
-        return new ResponseEntity<User>(user, HttpStatus.OK);
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
     @GetMapping("logout")
     public ResponseEntity<Void> logout(HttpServletRequest request){
         sessionManager.expire(request);
-        return new ResponseEntity<Void>(HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 
-    // 유저 id 입력하여 유저 반환
     @GetMapping("/user/{userId}")
     public ResponseEntity<User> getUser(@PathVariable int userId){
-        User user = userService.getUser(userId);
-        return new ResponseEntity<User>(user, HttpStatus.OK);
+        return new ResponseEntity<>(userService.getUser(userId), HttpStatus.OK);
     }
 
     // 모든 유저정보 반환
     @GetMapping("/users")
     public ResponseEntity<List<User>> getAllUser(){
-        List<User> userList = userService.getAllUser();
-        return new ResponseEntity<List<User>>(userList, HttpStatus.OK);
+        return new ResponseEntity<>(userService.getAllUser(), HttpStatus.OK);
     }
 
     // 유저 등록
@@ -82,16 +74,17 @@ public class UserGroupController {
     // 문제 : 백준에 없는 아이디를 입력했을 때와 정상적으로 등록되었을 때 구분 불가능
     // 비동기로 처리되기 때문에 리턴값이 없도록 구현..
     @GetMapping("/user/add/{userName}")
-    public ResponseEntity<Void> regist(@PathVariable String userName){
+    public ResponseEntity<?> regist(@PathVariable String userName){
 
+        // validation
         if (userService.getUserByName(userName) != null)
             return new ResponseEntity<Void>(HttpStatus.IM_USED);
         if (userService.getAllUser().size() >= 30)
             return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
 
-            userAddReloadDomain.userAddTask(userName);
-            User user = userService.getUserByName(userName);
-            return new ResponseEntity<Void>(HttpStatus.OK);
+        userAddReloadDomain.userAddTask(userName);
+        User user = userService.getUserByName(userName);
+        return new ResponseEntity<>(user.getUserId(), HttpStatus.OK);
     }
 
 
@@ -101,18 +94,17 @@ public class UserGroupController {
     // 그룹이 없으면 NO_CONTENT
     @GetMapping("/group") // 로그인된 유저의 그룹 정보 가져오기
     public ResponseEntity<?> getGroups(HttpServletRequest request){
-        int userId = (Integer) sessionManager.getSession(request);
+        int userId = getCurrentUserId(request);
         List<Group> list = groupService.getGroups(userId);
         list.forEach(group -> group.setPassword(group.getPassword().replaceAll(".", "*")));
 
-        if (list == null) return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
-        return new ResponseEntity<List<Group>>(list, HttpStatus.OK);
+        if (list.isEmpty()) return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+        return new ResponseEntity<>(list, HttpStatus.OK);
     }
 
     @GetMapping("/user/loginuser") // 로그인된 유저의 그룹 정보 가져오기
     public ResponseEntity<User> getLoginUser(HttpServletRequest request){
-        User user = userService.getUser((Integer) sessionManager.getSession(request));
-        return new ResponseEntity<User>(user, HttpStatus.OK);
+        return new ResponseEntity<>(userService.getUser((Integer) sessionManager.getSession(request)), HttpStatus.OK);
     }
 
 
@@ -123,20 +115,22 @@ public class UserGroupController {
     // 해당 유저에가 이미 3개의 그룹에 가입되어있으면 FORBIDDEN
     @PostMapping("/group/add")
     public ResponseEntity<?> addGroup(@RequestBody Group group, HttpServletRequest request){
-        User user = userService.getUser((Integer) sessionManager.getSession(request));
+        int userId = getCurrentUserId(request);
 
         if (group.getPassword().length() < 4) return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
 
-        int result = groupService.makeGroup(user, group);
+        int result = groupService.makeGroup(userId, group);
         if (result == 0) return new ResponseEntity<Void>(HttpStatus.FORBIDDEN);
         if (result == -1) return new ResponseEntity<Void>(HttpStatus.IM_USED);
 
-        groupService.addUser(group, user.getUserId());
+        groupService.addUser(group.getId(), userId);
         group = groupService.getGroupByName(group.getGroupName());
         group.setPassword(group.getPassword().replaceAll(".", "*"));
 
-        return new ResponseEntity<Group>(group, HttpStatus.OK);
+        return new ResponseEntity<>(group, HttpStatus.OK);
     }
+
+
 
     // 그룹 탈퇴
     // 그룹 탈퇴 성공시 OK, 1
@@ -144,33 +138,28 @@ public class UserGroupController {
     // 그룹 탈퇴로 인해 그룹 삭제시 OK, -1 리턴;
     @GetMapping("/group/leave/{groupId}") // 그룹 탈퇴
     public ResponseEntity<?> leaveGroup(@PathVariable int groupId, HttpServletRequest request){
-        Group group = groupService.getGroup(groupId);
-        User user = userService.getUser((Integer) sessionManager.getSession(request));
-        int result = groupService.removeUser(group.getId(), user.getUserId());
-        if(result == 0) new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
+        int userId = getCurrentUserId(request);
+        int result = groupService.removeUser(groupId, userId);
+        if (result == 0) new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 
         // 만약 탈퇴한 유저가 마지막 유저였다면 그룹을 삭제
-        if (groupService.getUsers(group.getId()).size() == 0){
-            groupService.removeGroup(group.getId());
+        if (groupService.getUsers(groupId).isEmpty()){
+            groupService.removeGroup(groupId);
             result = -1;
         }
 
-        return new ResponseEntity<Integer>(result, HttpStatus.OK);
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
 
     // 관리자 페이지 기능
-
     // 관리자 권한 확인 (비밀번호로 확인)
     // login 여부 boolean 반환
     @PostMapping("/group/admin")
     public ResponseEntity<Boolean> adminLogin(@RequestBody Map<String, Object> requestMap){
-        Group group = groupService.getGroup(mapper.convertValue(requestMap.get("group"), Integer.class));
+        int groupId = mapper.convertValue(requestMap.get("group"), Integer.class);
         String inputPassword = mapper.convertValue(requestMap.get("password"), String.class);
-        if (groupService.adminValid(group.getId(), inputPassword)){
-            return new ResponseEntity<Boolean>(true, HttpStatus.OK);
-        }
-        return new ResponseEntity<Boolean>(false, HttpStatus.OK);
+        return new ResponseEntity<>(groupService.adminValid(groupId, inputPassword), HttpStatus.OK);
     }
 
     // 그룹 삭제
@@ -180,14 +169,13 @@ public class UserGroupController {
     public ResponseEntity<?> deleteGroup(@PathVariable int groupId){
         int result = groupService.removeGroup(groupId);
         if (result == 0) return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
-        return new ResponseEntity<Integer>(groupId, HttpStatus.OK);
+        return new ResponseEntity<>(groupId, HttpStatus.OK);
     }
 
     // 관리자 페이지에서 띄워줄 그룹 유저
     @GetMapping("/group/admin/{groupId}")
     public ResponseEntity<?> getUsers(@PathVariable int groupId){
-        List<User> list = groupService.getUsers(groupId);
-        return new ResponseEntity<List<User>>(list, HttpStatus.OK);
+        return new ResponseEntity<>(groupService.getUsers(groupId), HttpStatus.OK);
     }
 
     // 유저 추가
@@ -195,25 +183,30 @@ public class UserGroupController {
     // 유저 삭제 비정상 작동시 BAD_REQUEST
     @PostMapping("/group/admin/add-user")
     public ResponseEntity<?> addUserIntoGroup(@RequestBody Map<String, Object> requestMap){
-        Group group = groupService.getGroup(mapper.convertValue(requestMap.get("group"), Integer.class));
-        User user = userService.getUser(mapper.convertValue(requestMap.get("user"), Integer.class));
-        int result = groupService.addUser(group, user.getUserId());
+        int groupId = mapper.convertValue(requestMap.get("group"), Integer.class);
+        int userId = mapper.convertValue(requestMap.get("user"), Integer.class);
+        User result = groupService.addUser(groupId, userId);
 
-        if (result == 0) return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
-        return new ResponseEntity<User>(user, HttpStatus.OK);
+        if (result == null) return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
     // 유저 방출 (탈퇴와 구분되어있음)
     // 로그인된 유저를 포함해 최소 1명은 남아있을 수 있음으로 그룹 삭제는 이뤄지지 않음
-    // 정상 작동시 탈퇴시킨 유저 정보 반환
+    // 정상 작동시 탈퇴시킨 유저 id 반환
     // 유저 삭제 비정상 작동시 BAD_REQUEST
     @PostMapping("/group/admin/remove-user")
     public ResponseEntity<?> removeUser(@RequestBody Map<String, Object> requestMap){
-        Group group = groupService.getGroup(mapper.convertValue(requestMap.get("group"), Integer.class));
-        User user = userService.getUser(mapper.convertValue(requestMap.get("user"), Integer.class));
-        int result = groupService.removeUser(group.getId(), user.getUserId());
+        int groupId = mapper.convertValue(requestMap.get("group"), Integer.class);
+        int userId = mapper.convertValue(requestMap.get("user"), Integer.class);
+
+        int result = groupService.removeUser(groupId, userId);
         if (result == 0) return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
-        return new ResponseEntity<User>(user, HttpStatus.OK);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    private int getCurrentUserId(HttpServletRequest request) {
+        return (int) sessionManager.getSession(request);
     }
 
 }
